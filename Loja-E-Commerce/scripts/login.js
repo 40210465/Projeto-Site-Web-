4fb03cadda5ff53ddb35e529318166bd1f6f4b5f
Pedro Teixeira <pedromst2000@gmail.com>
2022-01-06 05:38:06 +0000

    alterações visuais no register form

---
+++ Loja-E-Commerce/scripts/login.js
@@ -0,0 +1,14 @@
+// Vasco coloca aqui a interatividade do login form 
+// Basicamente neste formulário aplica-se os mesmos métodos das funções 
+
+// Regras dos campos 
+
+// Username ou Email = validar com o seu username ou email caso contrário surgirá uma mensagem de erro "Verifique se já tem uma conta criada"
+// Username ou Email = validar com caracteres no caso de username ou conter @ no caso email caso contrário surgirá uma mensagem de erro "campos inválidos"
+// Password = validar com a sua password 
+
+
+
+
+
+
