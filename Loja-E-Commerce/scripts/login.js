e7dccc57f10a75627654f7c7ab7bf8fa9c86d486
40210465 <40210465@esmad.ipp.pt>
2022-01-08 01:22:08 +0000

    ajustes no register form e login form finalizado falta adicionar JS aos dois

--- Loja-E-Commerce/scripts/login.js
+++ Loja-E-Commerce/scripts/login.js
@@ -3,12 +3,23 @@
 
 // Regras dos campos 
 
-// Username ou Email = validar com o seu username ou email caso contrário surgirá uma mensagem de erro "Verifique se já tem uma conta criada"
-// Username ou Email = validar com letras(strings) no caso de username ou conter @ no caso email caso contrário surgirá uma mensagem de erro "campos inválidos"
-// Password = validar com a sua password guardada na localStorage
+// Dados guardados na local storage após o registro
+
+//  Email = validar com o seu email caso contrário surgirá uma mensagem de erro "email incorreto!!"
+// Password = validar com a sua password  caso contrário surgirá uma mensagem de erro "password incorreta!!"
+
+// Caso os dados sejam incorretos ou inexistentes surgirá um pop-up quando o user clicar no btn LOGIN "Esta conta não existe!!"
+// Caso os dados sejam válidos ou existentes surgirá um pop-up quando o user clicar no btn LOGIN "BEM-VINDO DE VOLTA!!"
+
+// Aparecer um pop-up ao clicar no "esqueçeu da password" com a seguinte mensagem:
+// "SERÁ ENVIADO PARA O SEU EMAIL A SUA NOVA PASSWORD !!"
 
 
+// RECURSOS ONLINE 
+// https://www.ti-enxame.com/pt/javascript/como-salvar-dados-de-um-formulario-com-armazenamento-local-em-html5/1073245592/
+// https://www.youtube.com/watch?v=i04nMOowrII 
 
 
-
-
+// Para te ajudar nos pop-ups ou para teres uma ideia do que eu pretendo !!
+// https://www.w3schools.com/js/js_popup.asp 
+// https://www.w3schools.com/howto/howto_js_popup.asp
