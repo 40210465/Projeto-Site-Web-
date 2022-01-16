fb0a6ec12e5cc664c4632e585018ee5d0a09d1e6
40210465 <40210465@esmad.ipp.pt>
2022-01-16 18:58:21 +0000

    desenvolvimento da area de destaques

--- Loja-E-Commerce/scripts/destaques.js
+++ Loja-E-Commerce/scripts/destaques.js
@@ -1,2 +1,32 @@
 // ADICIONAR JS UM CAROSSELL SLIDE DE IMAGENS DOS PRODUTOS DE DESTAQUES NA MAIN SECTION DA PAGINA 
 // ADICIONAR JS AO CARRINHO DE COMPRAS DE FORMA ADICIONAR ARTIGOS SHOPPING CART !!
+
+
+// função que permite alterar no evento do mouseover o texto para "COMPRAR"
+function SonyVR_mouse_Over(){
+    document.getElementById("changeText").innerHTML = "COMPRAR";
+}
+
+// função que permite alterar para default no mouseout
+function SonyVR_mouse_Out(){
+    document.getElementById("changeText").innerHTML = "SONY OCULOS VR";
+}
+// função que permite alterar no evento do mouseover o texto para "COMPRAR"
+function FIFA22_mouse_over(){
+    document.getElementById("changeText2").innerHTML = "COMPRAR";
+}
+
+// função que permite alterar para default no mouseout
+function FIFA22_mouse_out(){
+    document.getElementById("changeText2").innerHTML = "FIFA 22 EDIÇÃO LIMITADA XBOX SERIES";
+}
+
+// função que permite alterar no evento do mouseover o texto para "COMPRAR"
+function PS5_mouse_over(){
+    document.getElementById("changeText3").innerHTML = "COMPRAR";
+}
+
+// função que permite alterar para default no mouseout
+function PS5_mouse_out(){
+    document.getElementById("changeText3").innerHTML = "EXCLUSIVO PLAYSTATION 5";
+}
