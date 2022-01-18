e0f936c4a87eb8b6b1207af811ae6150d0bbb9f2
40210465 <40210465@esmad.ipp.pt>
2022-01-18 05:01:05 +0000

    desenvolvimento da página de detalhes PLAYSTATION VR

---
+++ Loja-E-Commerce/scripts/sony-vr-detail.js
@@ -0,0 +1,9 @@
+
+function changeText(){
+    document.getElementById("changeText").innerHTML = "COMPRAR";
+}
+
+function defaultText(){
+    document.getElementById("changeText").innerHTML = "ADICIONAR AO CARRINHO";
+}
+
