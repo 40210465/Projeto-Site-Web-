4bd488cde2dcaa94d2c5df8d19bd63396d2bd9ed
40210465 <40210465@esmad.ipp.pt>
2022-01-18 20:41:33 +0000

    página de detalhe finalizada !!

--- Loja-E-Commerce/scripts/sony-vr-detail.js
+++
@@ -1,9 +0,0 @@
-
-function changeText(){
-    document.getElementById("changeText").innerHTML = "COMPRAR";
-}
-
-function defaultText(){
-    document.getElementById("changeText").innerHTML = "ADICIONAR AO CARRINHO";
-}
-
