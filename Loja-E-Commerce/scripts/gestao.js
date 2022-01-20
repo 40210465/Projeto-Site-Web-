4f4e87762b8210fe542f1b2aca4ca445c703adcd
40210465 <40210465@esmad.ipp.pt>
2022-01-20 04:30:11 +0000

    ajustes na página de gestão da conta do user

---
+++ Loja-E-Commerce/scripts/gestao.js
@@ -0,0 +1,24 @@
+function AlertSecurity(){
+    var uservalue = confirm ("Deseja ativar o modo de segurança da sua conta ?");  
+    var yesawnser = alert("ATIVADO COM SUCESSO !!")
+    if(uservalue == True){
+        yesawnser = alert;
+    }
+    if(uservalue == False){
+        return confirm
+    }
+}
+
+
+
+function AlertShop(){
+    window.alert("Nenhuma compra efetuada !!");
+}
+
+function eliminarConta(){
+    var uservalue = confirm ("Tem a certeza que quer eliminar a conta ?");
+
+}
+
+
+
