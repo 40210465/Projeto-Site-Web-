62a3ff8dfd4fac56a61c31847052090491859737
40210465 <40210465@esmad.ipp.pt>
2022-01-21 16:43:10 +0000

    ajustes

---
+++ Loja-E-Commerce/scripts/xbox.js
@@ -0,0 +1,31 @@
+function addArtigo1() {
+    sessionStorage.clear()
+	sessionStorage.setItem("artigo1",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
+
+function addArtigo2() {
+	sessionStorage.setItem("artigo2",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
+
+function addArtigo3() {
+	sessionStorage.setItem("artigo3",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
+
+function addArtigo4() {
+    sessionStorage.clear()
+	sessionStorage.setItem("artigo4",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
+
+function addArtigo5() {
+	sessionStorage.setItem("artigo5",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
+
+function addArtigo6() {
+	sessionStorage.setItem("artigo6",1);
+  document.getElementById("lblCartCount").innerHTML=sessionStorage.length;
+}
